6010d319a85309b90e7344689a53e6fff9abfb75
Michele Farruggio <michele.farruggio@gmail.com>
2025-04-07 16:50:00 +0100

    assignment 4.2

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from London!");
+    expect(hello.hello()).toEqual("Hello World from the United Kingdom!");
   });
 });
